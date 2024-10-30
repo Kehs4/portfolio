3690e586d94dbae77f7aceec4782cfc03ce1a036
Kleyton Holanda <kleytonholanda1@hotmail.com>
2024-10-29 23:35:05 -0300

    add elements 1.5

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const prev = document.getElementById('prev-btn')
 const next = document.getElementById('next-btn')
 const list = document.getElementById('item-list')
-const itemWidth = 150
-const padding = 10
+const itemWidth = 440
 
 prev.addEventListener('click', ()=>{
-    list.scrollLeft -= (itemWidth + padding)
+    list.scrollLeft -= (itemWidth)
 })
 
 next.addEventListener('click', ()=>{
-    list.scrollLeft += (itemWidth + padding)
+    list.scrollLeft += (itemWidth)
 })
